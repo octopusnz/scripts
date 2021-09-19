4f4f421298d55d4a06671cd4aab50087544f4937
Jacob Doherty <dev@theflat.gen.nz>
2021-09-19 17:42:02 +1200

    fixed git some more

--- updateotron.sh
+++ updateotron.sh
@@ -725,23 +725,21 @@ startup(){
     fi
 
     if [[ "${many_dir}" == "${val_source_dir}" ]] || [[ "${many_dir%/}" == "${val_source_dir}" ]]; then
-        cd "${many_dir}"
-        git fetch
-        if $(git rev-parse HEAD) != "$(git rev-parse '@{u}')"; then
-            val_upgrade=1
-        fi
-        cd "${script_dir}"
+      cd "${many_dir}"
+      git fetch
+      if [[ $(git rev-parse HEAD) != "$(git rev-parse @\{u\})" ]]; then
+        val_upgrade=1
+      fi
+      cd "${script_dir}"
     fi
 
     if [[ "${many_dir}" == "${shell_source_dir}" ]] || [[ "${many_dir%/}" == "${shell_source_dir}" ]]; then
-      if ! git -C "${many_dir}" diff --quiet remotes/origin/master; then
-          cd "${many_dir}"
-        git fetch
-        if $(git rev-parse HEAD) != "$(git rev-parse '@{u}')"; then
-            shell_upgrade=1
-        fi
-        cd "${script_dir}"
+      cd "${many_dir}"
+      git fetch
+      if [[ $(git rev-parse HEAD) != "$(git rev-parse @\{u\})" ]]; then
+        shell_upgrade=1
       fi
+      cd "${script_dir}"
     fi
   done
 
