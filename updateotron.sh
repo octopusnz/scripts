4432de91c50c6eccc7a0bc2435887a670f5a0b16
Jacob Doherty <dev@theflat.gen.nz>
2021-10-02 17:18:59 +1300

    inline git

--- updateotron.sh
+++ updateotron.sh
@@ -726,18 +726,16 @@ startup(){
     fi
 
     if [[ "${many_dir}" == "${val_source_dir}" ]] || [[ "${many_dir%/}" == "${val_source_dir}" ]]; then
-      cd "${many_dir}"
-      git fetch
-      if [[ $(git rev-parse HEAD) != "$(git rev-parse @\{u\})" ]]; then
+      git -C "${many_dir}" fetch
+      if [[ $(git -C "${many_dir}" rev-parse HEAD) != "$(git -C "${many_dir}" rev-parse @\{u\})" ]]; then
         val_upgrade=1
       fi
       cd "${script_dir}"
     fi
 
     if [[ "${many_dir}" == "${shell_source_dir}" ]] || [[ "${many_dir%/}" == "${shell_source_dir}" ]]; then
-      cd "${many_dir}"
-      git fetch
-      if [[ $(git rev-parse HEAD) != "$(git rev-parse @\{u\})" ]]; then
+      git -C "${many_dir}" fetch
+      if [[ $(git -C "${many_dir}" rev-parse HEAD) != "$(git -C "${many_dir}" rev-parse @\{u\})" ]]; then
         shell_upgrade=1
       fi
       cd "${script_dir}"
